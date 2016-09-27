a92d8733a3611ac8daa5029f88bf03da97c1bf1c
Kamil Tyliszczak <kamil@univem.edu.br>
2016-09-27 22:47:27 +0000

    Progress until 27.09.16

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   namespace :api do
+    namespace :v1 do
+      resources :login, param: :login
     end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
